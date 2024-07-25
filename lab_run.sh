735a4f2c0c3f620b6aa1f6933c873441443fd31a
ことみや <115519148+Kotomiya07@users.noreply.github.com>
2024-07-26 01:22:20 +0900

    fix run

--- lab_run.sh
+++ lab_run.sh
@@ -22,7 +22,7 @@ if [[ $MODE == train ]]; then
 	echo "==> Training LDDGAN"
 
 	if [[ $DATASET == cifar10 ]]; then
-		python3 train_ldgan_lab.py --dataset cifar10 --exp atn32_g122_2block_d3_Recloss_SmL_128 --num_channels 4 --num_channels_dae 128 --num_timesteps 4 \
+		python3 train_ldgan_lab.py --dataset cifar10 --exp atn32_g122_2block_d3_Recloss_SmL_256 --num_channels 4 --num_channels_dae 128 --num_timesteps 4 \
 			--num_res_blocks 2 --batch_size 256 --num_epoch 2000 --ngf 64 --nz 100 --z_emb_dim 256 --n_mlp 4 --embedding_type positional \
 			--use_ema --ema_decay 0.9999 --r1_gamma 0.02 --lr_d 1.25e-4 --lr_g 1.6e-4 --lazy_reg 15 \
 			--ch_mult 1 2 2 --save_content --datadir ./data/cifar-10 \
@@ -35,7 +35,7 @@ if [[ $MODE == train ]]; then
 			--sigmoid_learning
 
 	elif [[ $DATASET == cifar10_cond ]]; then
-		python3 train_ldgan_lab.py --dataset cifar10 --exp atn32_g122_2block_d3_Recloss_SmL_128 --num_channels 4 --num_channels_dae 128 --num_timesteps 4 \
+		python3 train_ldgan_lab.py --dataset cifar10 --exp atn32_g122_2block_d3_Recloss_SmL_256_cond --num_channels 4 --num_channels_dae 128 --num_timesteps 4 \
 			--num_res_blocks 2 --batch_size 256 --num_epoch 2000 --ngf 64 --nz 100 --z_emb_dim 256 --n_mlp 4 --embedding_type positional \
 			--use_ema --ema_decay 0.9999 --r1_gamma 0.02 --lr_d 1.25e-4 --lr_g 1.6e-4 --lazy_reg 15 \
 			--ch_mult 1 2 2 --save_content --datadir ./data/cifar-10 \
